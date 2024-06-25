0a4adfbab492d7e96ea3c14d4781611b1205b967
Jeú <jeufrancosilva@gmail.com>
2024-06-25 07:26:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+
+IConfiguration configuration = builder.Build();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine();
+Console.WriteLine("User Secrets");
+Console.WriteLine("------------");
+Console.ForegroundColor = ConsoleColor.Magenta;
+var apiKeyValue = configuration["ApiKey"] ?? string.Empty;
+Console.WriteLine($"ApiKey: {apiKeyValue}");
+Console.WriteLine();
